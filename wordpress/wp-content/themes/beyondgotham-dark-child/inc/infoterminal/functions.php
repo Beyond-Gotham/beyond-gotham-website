46c87aab8d8179091cd37c7521734505d4ab8155
saschi161 <saschi161@gmail.com>
2025-10-23 18:28:33 +0200

    Create functions.php

---
+++ wordpress/wp-content/themes/beyondgotham-dark-child/inc/infoterminal/functions.php
@@ -0,0 +1,3 @@
+// Am Ende der functions.php hinzufügen:
+require_once get_stylesheet_directory() . '/inc/infoterminal/custom-post-type-case-studies.php';
+require_once get_stylesheet_directory() . '/inc/infoterminal/functions-integration.php';
